d84bf41391f73a9cc4761d8f44a57fa04b5ec5c7
widua <mikolaj.widla@gmail.com>
2025-02-23 22:38:16 +0100

    Changed way the history is loaded, using thymeleaf templates, and HTMX get

--- src/main/kotlin/dev/widua/chatapp/controller/HistoryController.kt
+++ src/main/kotlin/dev/widua/chatapp/controller/HistoryController.kt
@@ -1,18 +1,21 @@
 package dev.widua.chatapp.controller
 
-import dev.widua.chatapp.chat.OutputMessage
 import dev.widua.chatapp.chat_history.MessageHistoryService
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestParam
 
-@RestController
+@Controller
 @RequestMapping("/history")
 class HistoryController(val historyService: MessageHistoryService) {
 
     @GetMapping("/getMessages")
-    fun getHistory(): List<OutputMessage>{
-        return historyService.getMessages();
+    fun getHistory(model: Model, @RequestParam username: String): String {
+        model.addAttribute("messages", historyService.getMessages())
+        model.addAttribute("username", username)
+        return "fragments/messages"
     }
 
 }
